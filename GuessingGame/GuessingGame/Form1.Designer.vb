bf378e7b8690b7234d3aa2ac0b7c401c5de849ff
lentø <33175224+LentoKun@users.noreply.github.com>
2019-09-26 19:33:05 -0700

    Add files via upload

---
+++ GuessingGame/GuessingGame/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInfo = New System.Windows.Forms.Button()
+        Me.btnHelp = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.lblGuess = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.txtGuess = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnInfo
+        '
+        Me.btnInfo.BackColor = System.Drawing.Color.White
+        Me.btnInfo.Font = New System.Drawing.Font("Monospac821 BT", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInfo.Location = New System.Drawing.Point(28, 182)
+        Me.btnInfo.Name = "btnInfo"
+        Me.btnInfo.Size = New System.Drawing.Size(260, 50)
+        Me.btnInfo.TabIndex = 2
+        Me.btnInfo.Text = "Information"
+        Me.btnInfo.UseVisualStyleBackColor = False
+        '
+        'btnHelp
+        '
+        Me.btnHelp.BackColor = System.Drawing.Color.White
+        Me.btnHelp.Font = New System.Drawing.Font("Monospac821 BT", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHelp.Location = New System.Drawing.Point(28, 247)
+        Me.btnHelp.Name = "btnHelp"
+        Me.btnHelp.Size = New System.Drawing.Size(260, 50)
+        Me.btnHelp.TabIndex = 3
+        Me.btnHelp.Text = "Help"
+        Me.btnHelp.UseVisualStyleBackColor = False
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.BackColor = System.Drawing.Color.White
+        Me.btnSubmit.Enabled = False
+        Me.btnSubmit.Font = New System.Drawing.Font("Monospac821 BT", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(28, 113)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(260, 50)
+        Me.btnSubmit.TabIndex = 1
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = False
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.White
+        Me.btnStart.Font = New System.Drawing.Font("Monospac821 BT", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnStart.Location = New System.Drawing.Point(29, 113)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(260, 50)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'lblNumber
+        '
+        Me.lblNumber.AutoSize = True
+        Me.lblNumber.Font = New System.Drawing.Font("Segoe UI Semibold", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumber.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblNumber.Location = New System.Drawing.Point(22, 23)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(267, 40)
+        Me.lblNumber.TabIndex = 5
+        Me.lblNumber.Text = "Enter your Number"
+        Me.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGuess
+        '
+        Me.lblGuess.AutoSize = True
+        Me.lblGuess.Font = New System.Drawing.Font("Segoe UI Semibold", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGuess.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblGuess.Location = New System.Drawing.Point(22, 23)
+        Me.lblGuess.Name = "lblGuess"
+        Me.lblGuess.Size = New System.Drawing.Size(236, 40)
+        Me.lblGuess.TabIndex = 6
+        Me.lblGuess.Text = "Enter your Guess"
+        Me.lblGuess.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumber.Location = New System.Drawing.Point(28, 66)
+        Me.txtNumber.MaxLength = 3
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtNumber.Size = New System.Drawing.Size(260, 35)
+        Me.txtNumber.TabIndex = 0
+        Me.txtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtGuess.Location = New System.Drawing.Point(28, 66)
+        Me.txtGuess.MaxLength = 3
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(260, 35)
+        Me.txtGuess.TabIndex = 0
+        Me.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(314, 316)
+        Me.Controls.Add(Me.txtGuess)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.lblGuess)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.btnHelp)
+        Me.Controls.Add(Me.btnInfo)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Guessing Game"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnInfo As System.Windows.Forms.Button
+    Friend WithEvents btnHelp As System.Windows.Forms.Button
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents lblNumber As System.Windows.Forms.Label
+    Friend WithEvents lblGuess As System.Windows.Forms.Label
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents txtGuess As System.Windows.Forms.TextBox
+
+End Class
---
+++ GuessingGame/GuessingGame/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class frmMain
+
+    Dim Count = 0
+
+    Private Sub btnInfo_Click(sender As Object, e As EventArgs) Handles btnInfo.Click
+        MsgBox("Allen Baluyot", vbInformation, "Information")
+    End Sub
+
+    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        MsgBox("(1) Enter any number to Start the game." & Chr(13) & "Note: Letters and Symbols are not allowed." & Chr(13) & "" & Chr(13) & "(2) Press the Start button to Play." & Chr(13) & "Note: You need to Input or Enter any Number to Play." & Chr(13) & "" & Chr(13) & "(3) The Main Objective here is to Guess any Number." & Chr(13) & "Note: Only 5 Chances if you not Guess any Number." & Chr(13) & "" & Chr(13) & "(4) If you Guess the Number, You Won." & Chr(13) & "     If Not, You go back to Play Again.", vbInformation, "How to Play?")
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If txtNumber.Text = "" Then
+            MsgBox("Please Input A Number", vbCritical, "Error")
+            txtNumber.Focus()
+        Else
+            MsgBox("You Only Have 5 Chances to Guess" & Chr(13) & "We Gonna Start The Game, Goodluck", vbInformation, "Lets Play")
+            Me.Width = 330
+            Me.Height = 250
+
+            lblGuess.Visible = True
+            btnSubmit.Visible = True
+            txtGuess.Visible = True
+
+            lblNumber.Visible = False
+            btnStart.Visible = False
+            btnInfo.Visible = False
+            btnHelp.Visible = False
+            txtNumber.Visible = False
+
+        End If
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If txtGuess.Text = txtNumber.Text Then
+            MsgBox("You Guessed the Number, Congratulations", vbInformation, "Hey, Congratulations!")
+            Count = 0
+            Me.Width = 330
+            Me.Height = 355
+            lblNumber.Visible = True
+            btnStart.Visible = True
+            btnInfo.Visible = True
+            btnHelp.Visible = True
+            txtNumber.Visible = True
+
+            lblGuess.Visible = False
+            btnSubmit.Visible = False
+            txtGuess.Visible = False
+
+            txtGuess.Clear()
+            txtNumber.Clear()
+            txtNumber.Focus()
+        Else
+            MsgBox("Sorry Wrong Guess" & Chr(13) & "Please Try Again", vbInformation, "Wrong Guess")
+            Count = Count + 1
+
+            txtGuess.Clear()
+            txtGuess.Focus()
+
+            If Count >= 5 Then
+                MsgBox("Game Over! " & Chr(13) & "The Guess Number is (" + txtNumber.Text + ") Better luck next time!", vbInformation, "Gameover")
+
+                txtGuess.Clear()
+                txtGuess.Focus()
+                Count = 0
+                Me.Width = 330
+                Me.Height = 355
+
+                lblNumber.Visible = True
+                btnStart.Visible = True
+                btnInfo.Visible = True
+                btnHelp.Visible = True
+                txtNumber.Visible = True
+
+                lblGuess.Visible = False
+                btnSubmit.Visible = False
+                txtGuess.Visible = False
+
+                txtGuess.Clear()
+                txtNumber.Clear()
+                txtNumber.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblGuess.Visible = False
+        btnSubmit.Visible = False
+        txtGuess.Visible = False
+        txtNumber.Focus()
+    End Sub
+
+    Private Sub txtNumber_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNumber.KeyPress
+        If Char.IsDigit(e.KeyChar) Or e.KeyChar = Chr(8) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtGuess_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtGuess.KeyPress
+        If Char.IsDigit(e.KeyChar) Or e.KeyChar = Chr(8) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtGuess_TextChanged(sender As Object, e As EventArgs) Handles txtGuess.TextChanged
+        If txtGuess.Text = "" Then
+            btnSubmit.Enabled = False
+        Else
+            btnSubmit.Enabled = True
+        End If
+    End Sub
+
+    Private Sub txtNumber_TextChanged(sender As Object, e As EventArgs) Handles txtNumber.TextChanged
+
+    End Sub
+End Class
