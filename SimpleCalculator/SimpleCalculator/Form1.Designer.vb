a13a4580565d8ff9908ced3d4b3d6e8be5a548e7
lentø <33175224+LentoKun@users.noreply.github.com>
2019-09-26 19:35:20 -0700

    Add files via upload

---
+++ SimpleCalculator/SimpleCalculator/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSub = New System.Windows.Forms.Button()
+        Me.btnMul = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnMod = New System.Windows.Forms.Button()
+        Me.btnInfo = New System.Windows.Forms.Button()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.lblNum2 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(23, 125)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(84, 32)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSub
+        '
+        Me.btnSub.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSub.Location = New System.Drawing.Point(23, 163)
+        Me.btnSub.Name = "btnSub"
+        Me.btnSub.Size = New System.Drawing.Size(84, 39)
+        Me.btnSub.TabIndex = 1
+        Me.btnSub.Text = "-"
+        Me.btnSub.UseVisualStyleBackColor = True
+        '
+        'btnMul
+        '
+        Me.btnMul.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMul.Location = New System.Drawing.Point(113, 125)
+        Me.btnMul.Name = "btnMul"
+        Me.btnMul.Size = New System.Drawing.Size(93, 32)
+        Me.btnMul.TabIndex = 2
+        Me.btnMul.Text = "*"
+        Me.btnMul.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDiv.Location = New System.Drawing.Point(113, 163)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(93, 39)
+        Me.btnDiv.TabIndex = 3
+        Me.btnDiv.Text = "/"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(212, 163)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(88, 39)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnMod
+        '
+        Me.btnMod.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMod.Location = New System.Drawing.Point(212, 125)
+        Me.btnMod.Name = "btnMod"
+        Me.btnMod.Size = New System.Drawing.Size(88, 32)
+        Me.btnMod.TabIndex = 5
+        Me.btnMod.Text = "Mod"
+        Me.btnMod.UseVisualStyleBackColor = True
+        '
+        'btnInfo
+        '
+        Me.btnInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInfo.Location = New System.Drawing.Point(94, 223)
+        Me.btnInfo.Name = "btnInfo"
+        Me.btnInfo.Size = New System.Drawing.Size(126, 38)
+        Me.btnInfo.TabIndex = 6
+        Me.btnInfo.Text = "Information"
+        Me.btnInfo.UseVisualStyleBackColor = True
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(50, 38)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 7
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(187, 48)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.ReadOnly = True
+        Me.txtResult.Size = New System.Drawing.Size(100, 20)
+        Me.txtResult.TabIndex = 8
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(50, 79)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 9
+        '
+        'lblNum1
+        '
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.BackColor = System.Drawing.Color.Transparent
+        Me.lblNum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum1.ForeColor = System.Drawing.Color.Transparent
+        Me.lblNum1.Location = New System.Drawing.Point(63, 19)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(86, 16)
+        Me.lblNum1.TabIndex = 10
+        Me.lblNum1.Text = "1st Number"
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.BackColor = System.Drawing.Color.Transparent
+        Me.lblNum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblNum2.Location = New System.Drawing.Point(63, 63)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(91, 16)
+        Me.lblNum2.TabIndex = 11
+        Me.lblNum2.Text = "2nd Number"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.BackColor = System.Drawing.Color.Transparent
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblResult.Location = New System.Drawing.Point(209, 29)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(52, 16)
+        Me.lblResult.TabIndex = 12
+        Me.lblResult.Text = "Result"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(343, 320)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.btnInfo)
+        Me.Controls.Add(Me.btnMod)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btnMul)
+        Me.Controls.Add(Me.btnSub)
+        Me.Controls.Add(Me.btnAdd)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMain"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnSub As System.Windows.Forms.Button
+    Friend WithEvents btnMul As System.Windows.Forms.Button
+    Friend WithEvents btnDiv As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnMod As System.Windows.Forms.Button
+    Friend WithEvents btnInfo As System.Windows.Forms.Button
+    Friend WithEvents txtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtResult As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblNum1 As System.Windows.Forms.Label
+    Friend WithEvents lblNum2 As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+
+End Class
---
+++ SimpleCalculator/SimpleCalculator/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class frmMain
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        txtResult.Text = Val(txtNum1.Text) + Val(txtNum2.Text)
+    End Sub
+
+    Private Sub btnSub_Click(sender As Object, e As EventArgs) Handles btnSub.Click
+        txtResult.Text = Val(txtNum1.Text) - Val(txtNum2.Text)
+    End Sub
+
+    Private Sub btnMul_Click(sender As Object, e As EventArgs) Handles btnMul.Click
+        txtResult.Text = Val(txtNum1.Text) * Val(txtNum2.Text)
+    End Sub
+
+    Private Sub btnDiv_Click(sender As Object, e As EventArgs) Handles btnDiv.Click
+        txtResult.Text = Val(txtNum1.Text) / Val(txtNum2.Text)
+    End Sub
+
+    Private Sub btnMod_Click(sender As Object, e As EventArgs) Handles btnMod.Click
+        txtResult.Text = Val(txtNum1.Text) Mod Val(txtNum2.Text)
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNum1.Clear()
+        txtNum2.Clear()
+        txtResult.Clear()
+        txtNum1.Focus()
+    End Sub
+
+    Private Sub btnInfo_Click(sender As Object, e As EventArgs) Handles btnInfo.Click
+        MsgBox("Allen James Baluyot", vbInformation, "Information")
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lblResult.Click
+
+    End Sub
+End Class
